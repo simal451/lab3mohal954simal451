4c8b78b690fa8604158327b4f84a04723f4161c9
Simon Alsen <simal451@student.liu.se>
2021-09-11 11:20:00 +0200

    New commit.

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -6,7 +6,7 @@
 #' @examples
 #' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 1)
 #' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 3)
-#' @references \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#' @references \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm/}
 #' @export
 dijkstra <- function(graph, init_node) {
   nodes <- unique(graph[,1])
